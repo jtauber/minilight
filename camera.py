df0153586000db1f9f5fdf6f36b1f689430c2726
James Tauber <jtauber@jtauber.com>
2009-11-25 20:12:50 -0500

    little bit of code formatting and comments

--- camera.py
+++ camera.py
@@ -18,8 +18,15 @@ MAX_ANGLE = 160.0
 
 
 class Camera(object):
-
+    
+    # view_position
+    # view_direction
+    # view_angle (radians)
+    # right
+    # up
+    
     def __init__(self, position, direction, angle):
+        
         self.view_position = Vector3f(position)
         self.view_direction = Vector3f(direction).unitize()
         
@@ -27,6 +34,9 @@ class Camera(object):
             self.view_direction = Vector3f(0.0, 0.0, 1.0)
         
         self.view_angle = min(max(MIN_ANGLE, float(angle)), MAX_ANGLE) * (pi / 180.0)
+        
+        # calculate right and up
+        
         self.right = Vector3f(0.0, 1.0, 0.0).cross(self.view_direction).unitize()
         
         if self.right.is_zero():
@@ -35,18 +45,27 @@ class Camera(object):
         else:
             self.up = self.view_direction.cross(self.right).unitize()
     
+    
     def get_frame(self, scene, image):
+        
         raytracer = RayTracer(scene)
         aspect = float(image.height) / float(image.width)
         
         for y in range(image.height):
             for x in range(image.width):
+                
+                ## convert x, y into sample_direction
+                
                 x_coefficient = ((x + random()) * 2.0 / image.width) - 1.0
                 y_coefficient = ((y + random()) * 2.0 / image.height) - 1.0
-                offset = self.right * x_coefficient + self.up * (y_coefficient * aspect)
                 
+                offset = self.right * x_coefficient + self.up * (y_coefficient * aspect)
                 sample_direction = (self.view_direction + (offset * tan(self.view_angle * 0.5))).unitize()
+                
+                # calculate radiance from that direction
+                
                 radiance = array(list(raytracer.get_radiance(self.view_position, sample_direction)))
                 
+                # and add to image
+                
                 image.add_to_pixel(x, y, radiance)
-
