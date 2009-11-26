c38fbc38b5fd5a5d00e5f38ce14e944c2543a221
James Tauber <jtauber@jtauber.com>
2009-11-26 13:28:58 +0800

    code formatting

--- triangle.py
+++ triangle.py
@@ -13,26 +13,36 @@ from vector3f import Vector3f, ZERO, ONE, MAX
 
 TOLERANCE = 1.0 / 1024.0
 
-class Triangle(object):
 
+class Triangle(object):
+    
     def __init__(self, v0, v1, v2, r, e):
+        
         self.vertexs = map(Vector3f, [v0, v1, v2])
         self.edge0 = Vector3f(v1) - Vector3f(v0)
         self.edge3 = Vector3f(v2) - Vector3f(v0)
+        
         self.reflectivity = Vector3f(r).clamped(ZERO, ONE)
         self.emitivity = Vector3f(e).clamped(ZERO, MAX)
+        
         edge1 = Vector3f(v2) - Vector3f(v1)
         self.tangent = self.edge0.unitize()
         self.normal = self.tangent.cross(edge1).unitize()
+        
         pa2 = self.edge0.cross(edge1)
         self.area = sqrt(pa2.dot(pa2)) * 0.5
-
+    
+    
     def get_bound(self):
+        
         v2 = self.vertexs[2]
         bound = [v2.x, v2.y, v2.z, v2.x, v2.y, v2.z]
+        
         for j in range(3):
+            
             v0 = self.vertexs[0][j]
             v1 = self.vertexs[1][j]
+            
             if v0 < v1:
                 if v0 < bound[j]:
                     bound[j] = v0
@@ -43,11 +53,15 @@ class Triangle(object):
                     bound[j] = v1
                 if v0 > bound[j + 3]:
                     bound[j + 3] = v0
+            
             bound[j] -= (abs(bound[j]) + 1.0) * TOLERANCE
             bound[j + 3] += (abs(bound[j + 3]) + 1.0) * TOLERANCE
+        
         return bound
-
+    
+    
     def get_intersection(self, ray_origin, ray_direction):
+        
         e1x = self.edge0.x
         e1y = self.edge0.y
         e1z = self.edge0.z
@@ -92,10 +106,13 @@ class Triangle(object):
             return None
         
         return t
-
+    
+    
     def get_sample_point(self):
+        
         sqr1 = sqrt(random())
         r2 = random()
         a = 1.0 - sqr1
         b = (1.0 - r2) * sqr1
+        
         return self.edge0 * a + self.edge3 * b + self.vertexs[0]
