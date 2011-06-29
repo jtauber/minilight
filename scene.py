5c384dadc0c64ccc2ba60ee32918cb0b74cb32e9
James Tauber <jtauber@jtauber.com>
2011-06-28 23:41:34 -0400

    added back spatial index

--- scene.py
+++ scene.py
@@ -7,10 +7,10 @@
 
 
 from random import choice
+
+from spatialindex import SpatialIndex
 from triangle import Triangle
 from vector3f import Vector3f, ZERO, ONE, MAX
-
-MAX_FLOAT = float(2**1024 - 2**971)
 
 
 class Scene(object):
@@ -19,16 +19,20 @@ class Scene(object):
         self.sky_emission = Vector3f(sky_emission).clamped(ZERO, MAX)
         self.ground_reflection = Vector3f(ground_reflection).clamped(ZERO, ONE)
         
-        self.triangles = []
+        triangles = []
         self.emitters = []
         
         for v0, v1, v2, r, e in t:
             triangle = Triangle(v0, v1, v2, r, e)
-            self.triangles.append(triangle)
+            triangles.append(triangle)
             if not triangle.emitivity.is_zero() and triangle.area > 0.0:
                 self.emitters.append(triangle)
                 
-        print "loaded %d triangles (%d emitters)" % (len(self.triangles), len(self.emitters))
+        print "loaded %d triangles (%d emitters)" % (len(triangles), len(self.emitters))
+        
+        self.index = SpatialIndex(eye_position, triangles)
+        print "built spatial index (%d deep)" % self.index.deepest_level
+        self.get_intersection = self.index.get_intersection
         
     def get_emitter(self):
         if self.emitters:
@@ -46,17 +50,17 @@ class Scene(object):
         else:
             return self.sky_emission * self.ground_reflection
     
-    def get_intersection(self, ray_origin, ray_direction):
-        hit_object = hit_position = None
-        
-        nearest_distance = MAX_FLOAT
-        
-        for triangle in self.triangles:
-            distance = triangle.get_intersection(ray_origin, ray_direction)
-            
-            if distance and (distance < nearest_distance):
-                hit_object = triangle
-                hit_position = ray_origin + ray_direction * distance
-                nearest_distance = distance
-        
-        return hit_object, hit_position
+    # def get_intersection(self, ray_origin, ray_direction):
+    #     hit_object = hit_position = None
+    #     
+    #     nearest_distance = MAX_FLOAT
+    #     
+    #     for triangle in self.triangles:
+    #         distance = triangle.get_intersection(ray_origin, ray_direction)
+    #         
+    #         if distance and (distance < nearest_distance):
+    #             hit_object = triangle
+    #             hit_position = ray_origin + ray_direction * distance
+    #             nearest_distance = distance
+    #     
+    #     return hit_object, hit_position
